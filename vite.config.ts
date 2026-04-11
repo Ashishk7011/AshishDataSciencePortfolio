16cc6b0e011f46edd50b0373593ef1e7655a560b
Ashishk7011 <ashishk7011@gmail.com>
2026-04-11 11:43:25 +0530

    vite.config.ts changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/AshishPortfolio/",
+  base: "/AshishDataSciencePortfolio/",
   server: {
     host: "::",
     port: 8080,
