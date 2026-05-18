7b10c73e6c135547c0fcb401d40b875c2ef0dcab
Ashishk7011 <ashishk7011@gmail.com>
2026-05-18 11:38:00 +0530

    changes in skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -26,7 +26,7 @@ const Skills = () => {
     {
       icon: FileSpreadsheet,
       title: "Tools & Platforms",
-      skills: ["Ms Excel", "VS Code", "Jupyter", "MLflow", "DagsHub", "Docker", "FastAPI", "Flask", "GitHub", "Streamlit", "DVC", "MLflow", "DagsHub", "Python", "LangChain", "LangGraph", "Streamlit", "ChromaDB", "BM25", "ChromaDB", "OpenAI", "Ollama", "RAG"]
+      skills: ["Ms Excel", "VS Code", "Jupyter", "MLflow", "DagsHub", "Docker", "FastAPI", "Flask", "GitHub", "Streamlit", "DVC", "MLflow", "DagsHub", "LangChain", "LangGraph", "Streamlit", "ChromaDB", "BM25", "OpenAI", "Ollama", "RAG"]
     },
     {
       icon: Cloud,
